84548a34ec0be51363784ef2c000aeb15a50a17d
Daniel Choi <cys920622@gmail.com>
2017-03-18 18:02:10 -0700

    Create service for http requests. Add mock GET call that gets a table from the server and display it on the client.

--- client/server/routes/api.js
+++ client/server/routes/api.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+var mockTable = {
+  "entries": [
+    {"key1": "val1", "key2": "val2"},
+    {"key1": "val3", "key2": "val4"}
+  ]
+};
+
 /* GET api listing. */
 router.get('/', (req, res) => {
   res.send('api works')
@@ -8,7 +15,7 @@ router.get('/', (req, res) => {
 
 // Get all posts
 router.get('/posts', (req, res) => {
-  res.send('table')
+  res.send(mockTable)
 })
 
 module.exports = router;
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import {PostsService} from "./posts.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [PostsService]
 })
 export class AppComponent {
   title = 'app works!';
