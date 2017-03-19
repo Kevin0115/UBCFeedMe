1c9f87b8a81b25515715fc97cd1eaca2dd9af1bf
Daniel Choi <cys920622@gmail.com>
2017-03-18 17:29:02 -0700

    Add basic routing from ‘/‘ to ‘/posts’. Add script to package.json so we can just run “npm run build” to build and launch node server.

--- client/server/routes/api.js
+++ client/server/routes/api.js
@@ -3,7 +3,12 @@ const router = express.Router();
 
 /* GET api listing. */
 router.get('/', (req, res) => {
-  res.send('api works');
-});
+  res.send('api works')
+})
+
+// Get all posts
+router.get('/posts', (req, res) => {
+  res.send('table')
+})
 
 module.exports = router;
