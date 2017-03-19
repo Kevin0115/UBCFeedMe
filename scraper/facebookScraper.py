c2b110d766db8238fb322b75595c428c840874db
MathewMacDougall <mmacd97@gmail.com>
2017-03-18 21:13:24 -0700

    improved facebook event scraping. Now accepts a list of page names

--- scraper/facebookScraper.py
+++ scraper/facebookScraper.py
@@ -3,7 +3,6 @@ import requests
 import time
 import json
 
-accessToken = 'EAACEdEose0cBAJXZBGPbZAfzAUb1js9dqmsHsnJerinKhUIw2fzdSycYPJR7LwqaADJnadMNAON6Tv7fj7g8b4cAsOspteNFhERqPPyYpdYnLqqhwu42zo39zuwqiZCF1hn0dvJTJHdInZB2D37HYifyeNaUNd57ZC7c1NKrjL5SHznLEDmIBtRqD7VZB8KnIZD'
 filepath = 'universalAccessCode.txt'
 universalAccessCode = "800557763431706%7Cqpv6eBTlNwdkhxd4zyy1NbfxGFs"
 
@@ -12,24 +11,21 @@ universalAccessCode = "800557763431706%7Cqpv6eBTlNwdkhxd4zyy1NbfxGFs"
 #     universalAccessCode = file.read()
 
 #print(universalAccessCode)
-page_ids = ['1035693569787773']
-#page_ids = ['bbcnews/events']
+page_ids = ['UBCEngineers', 'susubc']
 
 events = []
 for id in page_ids:
-    event = requests.get("https://graph.facebook.com/v2.7/",
+    event = requests.get("https://graph.facebook.com/v2.8/",
                           params={
+                              "limit": 100,
                               "ids": id,
-                              "fields": "events.fields(id,name,start_time,description,place).since({0})".format(time.strftime("%Y-%m-%d")),
+                              "since": time.strftime("%Y-%m-%d"),
+                              "fields": "events.fields(id,name,start_time,end_time,description,place)",
                               "access_token": universalAccessCode,
                         }
                         )
 
-    #print(event.json())
     event = event.json()[id]['events']['data']
     events = events + event
 
 print(events)
-
-#"fields": "events.fields(id,name,start_time,description,place,type,category,ticket_uri,cover.fields(id,source),picture.type(large),attending_count,declined_count,maybe_count,noreply_count).since({0}),id,name,cover.fields(id,source),picture.type(large),location".format(
-                              #    time.strftime("%Y-%m-%d")),
